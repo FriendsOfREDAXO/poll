b00b8ae2e50bee180dcb2bc006cfe480134350c1
Jan Kristinus <jan.kristinus@yakamara.de>
2017-09-01 17:40:32 +0200

    Eigenes Hauptmenü für Umfragen inkl Tabellen

--- boot.php
+++ boot.php
@@ -1,5 +1,28 @@
 <?php
 
+if (rex::isBackend()) {
+    rex_extension::register('PACKAGES_INCLUDED', function ($params) {
+
+        $plugin = rex_plugin::get('yform', 'manager');
+
+        if ($plugin) {
+            $pages = $plugin->getProperty('pages');
+            $ycom_tables = ['rex_poll', 'rex_poll_option', 'rex_poll_vote'];
+
+            if (isset($pages) && is_array($pages)) {
+                foreach ($pages as $page) {
+                    if (in_array($page->getKey(), $ycom_tables)) {
+                        $page->setBlock('poll');
+                    }
+                }
+
+            }
+
+        }
+
+    });
+}
+
 rex_yform_manager_dataset::setModelClass('rex_poll', rex_poll::class);
 rex_yform_manager_dataset::setModelClass('rex_poll_option', rex_poll_option::class);
 rex_yform_manager_dataset::setModelClass('rex_poll_vote', rex_poll_vote::class);
