a2c1c3ac809c8fa5a5c6983fdd46336ea4224ffa
Thomas Skerbis <thomas.skerbis@mac.com>
2023-05-05 15:13:10 +0200

    prevent division by Zero

--- fragments/addons/poll/poll.php
+++ fragments/addons/poll/poll.php
@@ -7,9 +7,9 @@ use Poll\User;
 $poll = $this->poll;
 $hash = rex_request('hash', 'string') != '' ? rex_request('hash', 'string') : User::getHash();
 
-echo '<h1>{{ poll_title }}: '.rex_escape($poll->getTitle()).'</h1> ';
+echo '<h1>{{ poll_title }}: ' . rex_escape($poll->getTitle()) . '</h1> ';
 if ('' !== trim($poll->getDescription())) {
-    echo '<p>'.rex_escape(nl2br($poll->getDescription()), 'html_simplified').'</p> ';
+    echo '<p>' . rex_escape(nl2br($poll->getDescription()), 'html_simplified') . '</p> ';
 }
 
 echo $poll->getOutput();
@@ -24,51 +24,55 @@ if ($poll->showResult($hash)) {
         }
 
         $description = '';
-        if('' !== $question->getDescription()){
-            $description = '<div class="poll-description">'.$question->getDescription().'</div>';
+        if ('' !== $question->getDescription()) {
+            $description = '<div class="poll-description">' . $question->getDescription() . '</div>';
         }
 
         $picture = '';
-        if(rex_media::get($question->media)){
-            $picture = '<div class="poll-picture"><img src="/media/'.$question->media.'"/></div>';
+        if (rex_media::get($question->media)) {
+            $picture = '<div class="poll-picture"><img src="/media/' . $question->media . '"/></div>';
         }
 
         $link = '';
-        if('' != $question->getUrl()){
-            $link = '<div class="poll-link"><a href="'.$question->getUrl().'">mehr Informationen</a></div>';
+        if ('' != $question->getUrl()) {
+            $link = '<div class="poll-link"><a href="' . $question->getUrl() . '">mehr Informationen</a></div>';
         }
 
 
         $progressBar = [];
         $hitsAll = $question->getHits();
-        foreach ($choices as $choice) {
-            $hits = $choice->getHits();
-            $percent = (int)($hits / $hitsAll * 100);
+        if ($hitsAll != 0) {
+            foreach ($choices as $choice) {
+                $hits = $choice->getHits();
+                $percent = (int)($hits / $hitsAll * 100);
 
-            $progressBar[] =
-                '<div class="poll-progress-item" aria-valuenow="'.$percent.'" aria-valuemin="0" aria-valuemax="100">
-                    <span class="poll-progress-label">'.rex_escape($choice->getTitle()).'</span>
-                    <progress class="poll-progress-bar" value="'.$percent.'" max="100">'.$percent.'%</progress>
-                    <span class="poll-progress-value">'.$percent.'%<span>['.$hits.']</span></span>
+                $progressBar[] =
+                    '<div class="poll-progress-item" aria-valuenow="' . $percent . '" aria-valuemin="0" aria-valuemax="100">
+                    <span class="poll-progress-label">' . rex_escape($choice->getTitle()) . '</span>
+                    <progress class="poll-progress-bar" value="' . $percent . '" max="100">' . $percent . '%</progress>
+                    <span class="poll-progress-value">' . $percent . '%<span>[' . $hits . ']</span></span>
+                </div>';
+            }
+            $progressBarOut = '<div class="poll-progress">
+                    ' . implode('', $progressBar) . '
                 </div>';
         }
 
         $items[] =
             '<li>
-                <div class="poll-title">'.$question->getTitle().'</div>
-                '.$description.'
-                '.$picture.'
-                '.$link.'
-                <div class="poll-progress">
-                    '.implode('', $progressBar).'
-                </div>
+                <div class="poll-title">' . $question->getTitle() . '</div>
+                ' . $description . '
+                ' . $picture . '
+                ' . $link . '
+                ' . $progressBarOut . '
             </li>';
     }
 
+
     echo
-        '<div class="poll">
+    '<div class="poll">
             <h2>{{ poll_result }}</h2>
-            '.($poll->getHits() > 0 ? '<p>{{ poll_votes_taken }} '.$poll->getHits().'</p>' : '').'
-            <ul class="poll-result-list">'.implode('', $items).'</ul>
+            ' . ($poll->getHits() > 0 ? '<p>{{ poll_votes_taken }} ' . $poll->getHits() . '</p>' : '') . '
+            <ul class="poll-result-list">' . implode('', $items) . '</ul>
         </div>';
 }
