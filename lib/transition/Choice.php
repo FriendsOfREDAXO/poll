cbd59c150f1f5d0a2c2a106268a121264b4dbd66
christophboecker <christoph_boecker@t-online.de>
2025-07-31 11:40:00 +0200

    Übergangsklassen im alten Namespace

---
+++ lib/transition/Choice.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Hilfsweise für die Umstellung die Klasse im alten Namespace zur Verfügung stellen.
+ *
+ * @see https://github.com/orgs/FriendsOfREDAXO/discussions/40
+ */
+
+namespace Poll\Question;
+
+/**
+ * @deprecated 3.0.0 Aufrufe auf "FriendsOfRedaxo\Poll\Question\Choice" (neuer Namespace) umstellen
+ */
+class Choice extends \FriendsOfRedaxo\Poll\Question\Choice {}
